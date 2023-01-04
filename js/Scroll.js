9072bf2b7fdb07a812a70b9d95eaf5703ff4d10c
Lucas Funabashi <lucasfunabashi@gmail.com>
2023-01-04 14:15:43 -0300

    Atualizando novas modificacoes na parte de projetos, falta so fazer o ver mais funcionar

---
+++ js/Scroll.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '15px' });
+sr.reveal('.desenho', {duration : 3000, origin: 'right'})
+sr.reveal('.inicio-apresentacao', {duration : 3000, origin: 'left'})
+sr.reveal('.linkedin', {duration : 3000,  origin: 'left'})
+sr.reveal('.desenho-bg', {duration : 3000,  origin: 'left'})
+sr.reveal('.sobre-mim', {duration : 3000, origin:'right'})
+sr.reveal('.line-projects', {duration : 3000, origin:'bottom'})
+sr.reveal('.project-title', {duration : 3000, origin:'bottom'})
+sr.reveal('.git-repository', {duration : 3000, origin:'bottom'})
+sr.reveal('.conteiner-skills', {duration : 3000, origin:'left'})
+sr.reveal('.conteiner-descricao', {duration : 3000, origin:'right'})
---
+++ js/maquina_escrever.js
@@ -0,0 +1,40 @@
+const meuNome = document.querySelector('.meu-nome');
+const fName = meuNome.innerHTML;
+const botaoVerMais = document.querySelector('button');
+const habilidades = document.querySelectorAll('.hidder');
+const node = document.querySelector('#fundo')
+meuNome.innerHTML='';
+
+function cliqueVer (e) {
+    habilidades.forEach( (elemento) => {
+        if (elemento.classList.contains('hidder')) {
+            elemento.classList.remove('hidder');
+            elemento.classList.add('visivel');
+            botaoVerMais.innerHTML = 'Ver menos';
+        } else {
+            elemento.classList.add('hidder');
+            elemento.classList.remove('visivel');
+            botaoVerMais.innerHTML = 'Ver mais';
+        }
+    } )
+}
+
+function maisHabilidades (elemento) {
+    elemento.addEventListener('click', cliqueVer)
+}
+
+function typeWriter (nome) {
+    setTimeout( () => {
+    const meuNomeF = nome.split('');
+    meuNome.innerHTML='';
+    meuNomeF.forEach( (letra, index) => {
+        setTimeout(function (){
+            meuNome.innerHTML += letra;
+        }, 150 * index)
+    })}, 1500)
+    
+}
+
+/* typeWriter(fName); */
+maisHabilidades(botaoVerMais);
+
